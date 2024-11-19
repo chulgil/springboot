569376f3f4e5be2ee22114d66ce5b6170c4a88d1
CGLee <chulgil.lee@riman.com>
2024-11-19 17:06:03 +0900

    Chore: add gitignore

---
+++ spring-batch/src/main/java/me/chulgil/spring/springbatch/SpringBatchApplication.java
@@ -0,0 +1,8 @@
+package me.chulgil.spring.springbatch;
+
+public class SpringBatchApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
